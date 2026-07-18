b807ede390b57a70c57c25fbb29c71c7a4dcf726
CallumRider <caldude182@icloud.com>
2026-07-18 17:26:23 +0100

    Refactor photo carousel functionality in script.js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,67 @@
-const cadetPhotos = [
-    "assets/images/cadet1.jpg",
-    "assets/images/cadet2.jpg",
-    "assets/images/cadet3.jpg",
-    "assets/images/cadet4.jpg",
-    "assets/images/cadet5.jpg",
-    "assets/images/cadet6.jpg",
-    "assets/images/cadet7.jpg"
-];
+const track = document.getElementById("carousel-track");
+const dotsContainer = document.getElementById("carousel-dots");
 
+const totalPhotos = 7;
 let currentPhoto = 0;
+
+for(let i=0;i<totalPhotos;i++){
+
+    const dot=document.createElement("span");
+
+    if(i===0) dot.classList.add("active");
+
+    dot.onclick=()=>{
+        currentPhoto=i;
+        updateCarousel();
+        restartTimer();
+    };
+
+    dotsContainer.appendChild(dot);
+
+}
+
+function updateCarousel(){
+
+    track.style.transform=`translateX(-${currentPhoto*100}%)`;
+
+    document.querySelectorAll(".carousel-dots span").forEach((dot,index)=>{
+
+        dot.classList.toggle("active",index===currentPhoto);
+
+    });
+
+}
 
 function changePhoto(direction){
 
-    currentPhoto += direction;
+    currentPhoto+=direction;
 
-    if(currentPhoto < 0){
-        currentPhoto = cadetPhotos.length - 1;
-    }
+    if(currentPhoto<0) currentPhoto=totalPhotos-1;
 
-    if(currentPhoto >= cadetPhotos.length){
-        currentPhoto = 0;
-    }
+    if(currentPhoto>=totalPhotos) currentPhoto=0;
 
-    document.getElementById("cadet-photo").src = cadetPhotos[currentPhoto];
+    updateCarousel();
+
+    restartTimer();
+
+}
+
+let timer=setInterval(nextPhoto,5000);
+
+function nextPhoto(){
+
+    currentPhoto++;
+
+    if(currentPhoto>=totalPhotos) currentPhoto=0;
+
+    updateCarousel();
+
+}
+
+function restartTimer(){
+
+    clearInterval(timer);
+
+    timer=setInterval(nextPhoto,5000);
 
 }
